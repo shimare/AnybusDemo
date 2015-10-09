200866efbdbf04b3e03ffd00877b88c294a18ec5
shimare <shimare@sina.com>
2015-10-09 22:49:42 +0800

    create demo project

---
+++ AnybusTestDemo/main.c
@@ -0,0 +1,12 @@
+//#include "src\ABCC\INCLUDE\abcc.h"
+//#include "src\ABCC\INCLUDE\abcc_com.h"
+//#include "src\ABCC\INCLUDE\abcc_td.h"
+
+#include "abcc.h"
+#include "abcc_com.h"
+#include "abcc_td.h"
+
+int main(void)
+{
+	return 0;
+}
---
+++ AnybusTestDemo/src/main.c
@@ -0,0 +1,11 @@
+//#include "abcc_td.h"
+//#include "abcc_com.h"
+//#include "abcc.h"
+
+#include "abcc_td.h"
+
+int main(void)
+{
+	return 0;
+	// Nothing yet
+}
